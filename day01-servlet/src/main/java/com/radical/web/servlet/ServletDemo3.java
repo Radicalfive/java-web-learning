9f09d0ce5856e6bbc4aa7e161086929e5f843510
Radicalfive <1561548288@qq.com>
2022-02-26 19:46:13 +0800

    Day01-Servlet的学习

---
+++ day01-servlet/src/main/java/com/radical/web/servlet/ServletDemo3.java
@@ -0,0 +1,28 @@
+package com.radical.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author : radical
+ * @description :doGet和doPost
+ * @data : 2022/2/26
+ **/
+@WebServlet("/ServletDemo3")
+public class ServletDemo3 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet....");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost...");
+    }
+}
